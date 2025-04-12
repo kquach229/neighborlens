26539a2aa0363bdf8f230b7b2e9fdd5959ebd2e0
kquach229 <kquach229@gmail.com>
2025-04-12 12:57:39 -0400

    save

--- app/ideas/idea-details/[ideaId]/page.tsx
+++ app/ideas/idea-details/[ideaId]/page.tsx
@@ -4,7 +4,7 @@ import { Badge } from '@/components/ui/badge';
 import { Label } from '@/components/ui/label';
 import { prisma } from '@/lib/prisma';
 import { getTimeDifference } from '@/lib/utils';
-import { useState } from 'react';
+import { use } from 'react';
 
 const getIdea = async (ideaId) => {
   const idea = await prisma.idea.findUnique({
@@ -15,62 +15,67 @@ const getIdea = async (ideaId) => {
   return idea;
 };
 
-const IdeaDetails = async ({ params }) => {
+const IdeaDetails = async ({ params, searchParams }) => {
   const { ideaId } = await params;
   const idea = await getIdea(ideaId);
   const numberOfDaysSincePosting = getTimeDifference(idea.createdAt);
   const numberOfDaysSinceUpdated = getTimeDifference(idea.updatedAt);
+  const { isEditing } = await searchParams;
+  console.log('slkfjsfsjf', isEditing);
 
   return (
     <div className='min-h-[90vh] p-5 flex justify-between gap-10'>
-      <div>
-        <div className='flex justify-between items-cetner'>
+      {!isEditing && (
+        <div>
+          <div className='flex justify-between items-cetner'>
+            <div className='mt-10'>
+              <h1 className=''>{idea.title}</h1>
+              <span className='text-muted-foreground'>
+                Posted {numberOfDaysSincePosting} ago
+              </span>
+            </div>
+          </div>
+          <div className='mt-5'>
+            <Label className='text-xl'>Brief Description</Label>
+            <span>{idea?.briefDescription}</span>
+          </div>
+
+          <div className='mt-5'>
+            <Label className='text-xl'>Problem</Label>
+            <span>{idea?.problem}</span>
+          </div>
+
+          <div className='mt-5'>
+            <Label className='text-xl'>Solution</Label>
+            <span>{idea?.solution}</span>
+          </div>
+
+          <div className='mt-5'>
+            <Label className='text-xl'>Pricing Model</Label>
+            <span>{idea?.pricingModel}</span>
+          </div>
+
+          <div className='mt-5'>
+            <Label className='text-xl'>Pricing Details</Label>
+            <span>{idea?.pricingDetails}</span>
+          </div>
+
           <div className='mt-10'>
-            <h1 className=''>{idea.title}</h1>
-            <span className='text-muted-foreground'>
-              Posted {numberOfDaysSincePosting} ago
-            </span>
+            {idea.categories.map((category: string) => (
+              <Badge className='mr-2 mt-2 text-xs' key={category}>
+                {category.toUpperCase()}
+              </Badge>
+            ))}
           </div>
-        </div>
-        <div className='mt-5'>
-          <Label className='text-xl'>Brief Description</Label>
-          <span>{idea?.briefDescription}</span>
-        </div>
 
-        <div className='mt-5'>
-          <Label className='text-xl'>Problem</Label>
-          <span>{idea?.problem}</span>
+          {numberOfDaysSinceUpdated !== numberOfDaysSincePosting && (
+            <div className='text-xs text-muted-foreground text-right mt-2'>
+              Updated {numberOfDaysSinceUpdated} Ago
+            </div>
+          )}
         </div>
+      )}
 
-        <div className='mt-5'>
-          <Label className='text-xl'>Solution</Label>
-          <span>{idea?.solution}</span>
-        </div>
-
-        <div className='mt-5'>
-          <Label className='text-xl'>Pricing Model</Label>
-          <span>{idea?.pricingModel}</span>
-        </div>
-
-        <div className='mt-5'>
-          <Label className='text-xl'>Pricing Details</Label>
-          <span>{idea?.pricingDetails}</span>
-        </div>
-
-        <div className='mt-10'>
-          {idea.categories.map((category: string) => (
-            <Badge className='mr-2 mt-2 text-xs' key={category}>
-              {category.toUpperCase()}
-            </Badge>
-          ))}
-        </div>
-
-        {numberOfDaysSinceUpdated !== numberOfDaysSincePosting && (
-          <div className='text-xs text-muted-foreground text-right mt-2'>
-            Updated {numberOfDaysSinceUpdated} Ago
-          </div>
-        )}
-      </div>
       <div className='flex justify-end'>
         <div className='flex-1'>
           <ReusableEditFormButton
