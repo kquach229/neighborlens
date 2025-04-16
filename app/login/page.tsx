3ffe531e1c98bf0c711a17b7541a204286fc362e
kquach229 <kquach229@gmail.com>
2025-04-16 16:28:36 -0400

    login page change

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,12 +1,6 @@
 import auth from '@/auth';
 import SignInButton from '@/components/SignInButton';
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card';
+import { Glasses } from 'lucide-react';
 import { redirect } from 'next/navigation';
 import React from 'react';
 
@@ -15,20 +9,21 @@ const LoginPage = async () => {
   if (session?.user) redirect('/dashboard');
 
   return (
-    <div className='min-h-screen flex items-center justify-center bg-gradient-to-br from-white to-gray-100 px-4'>
-      <Card className='w-full max-w-md shadow-lg border-none'>
-        <CardHeader className='text-center'>
-          <CardTitle className='text-3xl font-bold tracking-tight'>
-            ValidatorLens
-          </CardTitle>
-          <CardDescription className='mt-2 text-gray-500'>
+    <div className='min-h-screen flex items-center justify-center px-4'>
+      <div className='w-full max-w-md text-center space-y-8'>
+        <div className='flex flex-col items-center space-y-4'>
+          <Glasses className='h-20 w-20' />
+          <h1 className='text-4xl font-bold tracking-tight'>ValidatorLens</h1>
+          <p className='max-w-sm'>
             Validate your startup ideas before investing your time and money.
-          </CardDescription>
-        </CardHeader>
-        <CardContent className='space-y-6'>
+          </p>
+        </div>
+
+        <div className='flex flex-col items-center gap-4'>
           <SignInButton />
-        </CardContent>
-      </Card>
+          <p className='text-sm'>You’ll be redirected to securely sign in.</p>
+        </div>
+      </div>
     </div>
   );
 };
