f6e0960f7389741a4576c0e47f68cb95ba24404e
kquach229 <kquach229@gmail.com>
2025-04-19 19:19:16 -0400

    add spinner

---
+++ app/loading.tsx
@@ -0,0 +1,10 @@
+import { PropagateLoader } from 'react-spinners';
+
+export default function Loading() {
+  // You can add any UI inside Loading, including a Skeleton.
+  return (
+    <div className='flex h-full items-center justify-center'>
+      <PropagateLoader />
+    </div>
+  );
+}
