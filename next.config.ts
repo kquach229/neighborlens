ed64e19d937d60a7b7e8a3d234202bcfb33123f0
kquach229 <kquach229@gmail.com>
2025-04-22 12:46:31 -0400

    modify next config file for pwa

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,55 @@
-// next.config.js
+/** @type {import('next').NextConfig} */
+
+const path = require('path');
+
+const isDev = process.env.NODE_ENV !== 'production';
+
 const withPWA = require('next-pwa')({
   dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
-  register: true,
-  skipWaiting: true,
+
+  disable: isDev,
+
+  buildExcludes: ['app-build-manifest.json'],
 });
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+const generateAppDirEntry = (entry: any) => {
+  const packagePath = require.resolve('next-pwa');
+
+  const packageDirectory = path.dirname(packagePath);
+
+  const registerJs = path.join(packageDirectory, 'register.js');
+
+  return entry().then((entries: any) => {
+    // Register SW on App directory, solution: https://github.com/shadowwalker/next-pwa/pull/427
+
+    if (entries['main-app'] && !entries['main-app'].includes(registerJs)) {
+      if (Array.isArray(entries['main-app'])) {
+        entries['main-app'].unshift(registerJs);
+      } else if (typeof entries['main-app'] === 'string') {
+        entries['main-app'] = [registerJs, entries['main-app']];
+      }
+    }
+
+    return entries;
+  });
+};
+
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+
+  reactStrictMode: true,
+
+  webpack(config: any) {
+    if (!isDev) {
+      const entry = generateAppDirEntry(config.entry);
+
+      config.entry = () => entry;
+    }
+
+    return config;
+  },
+};
 
 module.exports = withPWA(nextConfig);
