eeba2301618d219d483c9b42f05c2ff8e4ead31f
kquach229 <kquach229@gmail.com>
2025-04-22 14:41:46 -0400

    logo

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,23 +1,25 @@
 'use client';
-import React, { useEffect, useState } from 'react';
 import ThemeToggler from './ThemeToggler';
-import { Glasses, LogOut, User } from 'lucide-react';
+import { LogOut, User } from 'lucide-react';
 import Link from 'next/link';
-import { useDialogStore } from '@/stores/dialogStore';
 import { Avatar } from './ui/avatar';
 import { AvatarFallback, AvatarImage } from '@radix-ui/react-avatar';
 import { useSession, signOut } from 'next-auth/react';
 import { Popover, PopoverContent, PopoverTrigger } from './ui/popover';
 import SignInButton from './SignInButton';
-import UserCredits from './UserCredits';
 import CreateIdeaButton from './CreateIdeaButton';
 
-export const NavLogo = () => (
-  <Link href='/' className='inline-flex items-center gap-2 font-oleo'>
-    <h3>ValidateLens</h3>
-    <Glasses className='h-8 w-8' />
-  </Link>
-);
+import Logo from './Logo';
+
+export const NavLogo = () => {
+  return (
+    <Link href='/' className='inline-flex items-center gap-2 font-oleo'>
+      <div className='w-[150px] h-[50px] flex'>
+        <Logo />
+      </div>
+    </Link>
+  );
+};
 
 export const UserButton = () => {
   const session = useSession();
@@ -53,31 +55,31 @@ export const UserButton = () => {
 
 const Navbar = () => {
   const session = useSession();
-  const [credits, setCredits] = useState<number | null>(null);
+  // const [credits, setCredits] = useState<number | null>(null);
 
-  useEffect(() => {
-    const fetchCredits = async () => {
-      try {
-        const res = await fetch('/api/user/credits');
-        const data = await res.json();
-        setCredits(data.credits);
-      } catch (error) {
-        console.error('Failed to fetch credits', error);
-        setCredits(0);
-      }
-    };
+  // useEffect(() => {
+  //   const fetchCredits = async () => {
+  //     try {
+  //       const res = await fetch('/api/user/credits');
+  //       const data = await res.json();
+  //       setCredits(data.credits);
+  //     } catch (error) {
+  //       console.error('Failed to fetch credits', error);
+  //       setCredits(0);
+  //     }
+  //   };
 
-    if (session.data?.user) {
-      fetchCredits();
+  //   if (session.data?.user) {
+  //     fetchCredits();
 
-      const handleCreditsUpdated = () => fetchCredits();
-      window.addEventListener('credits-updated', handleCreditsUpdated);
+  //     const handleCreditsUpdated = () => fetchCredits();
+  //     window.addEventListener('credits-updated', handleCreditsUpdated);
 
-      return () => {
-        window.removeEventListener('credits-updated', handleCreditsUpdated);
-      };
-    }
-  }, [session.data?.user]);
+  //     return () => {
+  //       window.removeEventListener('credits-updated', handleCreditsUpdated);
+  //     };
+  //   }
+  // }, [session.data?.user]);
 
   return (
     <div className='flex justify-between items-center p-5 h-[10vh]'>
